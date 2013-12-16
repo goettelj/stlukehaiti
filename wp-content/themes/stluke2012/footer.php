976aeb6301fa2f8fd9797282e08ca00452f706b5
Jay Goettelmann <jay@liberatedmatter.com>
2013-12-16 14:49:13 -0500

    Added privacy policy to footer in place of Compassion Weavers.

--- wp-content/themes/stluke2012/footer.php
+++ wp-content/themes/stluke2012/footer.php
@@ -8,7 +8,7 @@
 			&nbsp;&nbsp;|&nbsp;&nbsp;
 			<a href="https://npo1.networkforgood.org/Donate/Donate.aspx?npoSubscriptionId=1005615&uniqueID=634713724539679837">DONATE</a>
 			&nbsp;&nbsp;|&nbsp;&nbsp;
-			<a href="http://www.compassionweavers.org/">COMPASSION WEAVERS</a>
+			<a href="privacy-policy">PRIVACY POLICY</a>
 		</span>
 		<span class="floatright">
 			&copy; <?php echo date('Y'); ?> St. Luke Foundation. All rights reserved.
