6359869370ac45c8dd94881ea45ef23112005e87
erineallen <erin@erin-allen.com>
2014-05-20 15:43:47 -0500

    Simplify last edit to functions.php

--- wp-content/themes/stluke2012/functions.php
+++ wp-content/themes/stluke2012/functions.php
@@ -4,37 +4,14 @@ add_theme_support( 'post-thumbnails' );
 set_post_thumbnail_size( 225, 1000 ); // 225 pixels wide by 1000 pixels tall, box resize mode
 add_image_size( 'single-post-thumbnail', 360, 1000 ); // Permalink thumbnail size
 
-//Add support for different single pages depending on category 
 
-
-/**
-* Define a constant path to our single template folder
-*/
-define(SINGLE_PATH, TEMPLATEPATH . '/single');
-
-/**
-* Filter the single_template with our custom function
-*/
-add_filter('single_template', 'my_single_template');
-
-/**
-* Single template function which will choose our template
-*/
-function my_single_template($single) {
-global $wp_query, $post;
-
-/**
-* Checks for single template by category
-* Check by category slug and ID
-*/
-foreach((array)get_the_category() as $cat) :
-
-if(file_exists(SINGLE_PATH . '/single-cat-' . $cat->slug . '.php'))
-return SINGLE_PATH . '/single-cat-' . $cat->slug . '.php';
-
-elseif(file_exists(SINGLE_PATH . '/single-cat-' . $cat->term_id . '.php'))
-return SINGLE_PATH . '/single-cat-' . $cat->term_id . '.php';
-
-endforeach;
+//Gets post cat slug and looks for single-[cat slug].php and applies it
+add_filter('single_template', create_function(
+	'$the_template',
+	'foreach( (array) get_the_category() as $cat ) {
+		if ( file_exists(TEMPLATEPATH . "/single-{$cat->slug}.php") )
+		return TEMPLATEPATH . "/single-{$cat->slug}.php"; }
+	return $the_template;' )
+);
 
 ?>
